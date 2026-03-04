f4831497474e9617cb1a7e614321f3343c909e6a
Ahmed Mustafa <ahmed@example.com>
2026-03-04 05:06:07 +0200

    dfgb

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,10 @@
 import app from '../index.js';
+import { connectDb } from '../Db/ConnectDb.js';
 
-export default app;
+export default async function handler(req, res) {
+  // تأكد إن قاعدة البيانات متوصلة قبل أي request
+  await connectDb();
+  
+  // خلي Express يعالج الـ request
+  app(req, res);
+}
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,9 @@
 import express from 'express';
-import { connectDb } from './Db/ConnectDb.js';
-import dotenv from 'dotenv';
 import userRouter from './routes/UserRoute.js';
 import cors from 'cors';
 import { cloudinaryConfig } from './config/cloudinary.js';
 import DoctorRoute from './routes/DoctorRoute.js';
 import AdminRoute from './routes/AdminRoute.js';
-
-dotenv.config();
 
 const app = express();
 
@@ -25,8 +21,5 @@ app.use('/api/adminRoute', AdminRoute);
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
-
-// Connect to DB
-await connectDb(); // لو Vercel شغال بـ Node 18+ يدعم await top-level
 
 export default app;
